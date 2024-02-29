ffe811e7643fe7e4745d3b49fe7a2aa1062f86dc
ismartNishant <nishantrajputall@gmail.com>
2024-03-01 00:08:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions =
+  require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated State ', store.getState())
+})
+
+// //cake
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
+
+
+// //icecream
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(10))
+
+store.dispatch(fetchUsers())
+// unsubscribe()
